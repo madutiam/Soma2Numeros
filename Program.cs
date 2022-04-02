954cf519ada55542eeb8da9acb1f174456a5299e
Conta.para.qualquer.coisa2005@gamail.com <Conta.para.qualquer.cois2005@gmail.com>
2022-04-01 21:55:42 -0300

    Pitagoras

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int n1, n2, soma;
+
+Console.Write("Digite um Número: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite outro Número: ");
+n2 = Convert.ToInt32(Console.ReadLine())!;
+
+soma = n1 + n2;
+Console.WriteLine($"Soma: {soma}");
